86b1c1387d3d029d76dc94bf997ad18d2c0bf16b
JA_X <envijs93@gmail.com>
2024-08-24 03:45:49 +0900

    feat: 스케치룸 기본 레이아웃 설정

--- src/pages/sketchRoom/sketchRoom.tsx
+++ src/pages/sketchRoom/sketchRoom.tsx
@@ -1,7 +1,17 @@
 import styles from './sketchRoom.module.scss';
 
 const SketchRoom = () => {
-  return <div className={styles.container}>스케치룸입니당</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoBox}>로고</div>
+      <div className={styles.header}>헤드라인</div>
+      <div className={styles.playArea}>
+        <div className={styles.participants}>참여자영역</div>
+        <div className={styles.canvas}>드로잉영역</div>
+        <div className={styles.chatting}>채팅영역</div>
+      </div>
+    </div>
+  );
 };
 
 export default SketchRoom;
