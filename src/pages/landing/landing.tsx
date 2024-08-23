3cd696ba22089fd35bfee15eafb2061a2ca7f723
JA_X <envijs93@gmail.com>
2024-08-24 05:29:24 +0900

    feat: 닉네임 입력 후 스케치룸 입장 시 firestore에 유저정보 저장 연동

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -1,17 +1,43 @@
 import styles from './landing.module.scss';
 import { useState } from 'react';
 import useNavigateClick from '../../hooks/useNavigateClick';
+import { createRandomNickName } from '../../utils/createRandomNickName';
+import { registerUserData } from '../../services/userService';
+import { userDataType } from '../../types/user/interface';
 
 const Landing = () => {
-  const [userName, setUserName] = useState('');
-  console.log(userName);
-  const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setUserName(e.target.value);
-  const handleNavigateClick = useNavigateClick();
+  const [userData, setUserData] = useState<userDataType>({ nickName: '' });
+  const navigateTo = useNavigateClick();
+
+  const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newUserData = {
+      ...userData,
+      nickName: e.target.value,
+    };
+    setUserData(newUserData);
+  };
+  const handleCreateRanDomNickNameClick = () => {
+    const newUserData = {
+      ...userData,
+      nickName: createRandomNickName(),
+    };
+    setUserData(newUserData);
+  };
+  // 유저 등록 후 스케치룸 이동
+  const handlePlayButtonClick = () => {
+    registerUserData(userData);
+    navigateTo('/sketchRoom');
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.registerBox}>
-        <input className={styles.inputBox} placeholder="Enter Your Name" onChange={handleUserNameChange} />
-        <button className={styles.playButton} onClick={() => handleNavigateClick('/sketchRoom')}>
+        <div className={styles.logoBox}>Logo</div>
+        <div className={styles.inputBox}>
+          <input placeholder="Enter Your NickName" value={userData.nickName} onChange={handleUserNameChange} />
+          <button onClick={handleCreateRanDomNickNameClick}>🎲</button>
+        </div>
+        <button className={styles.playButton} onClick={handlePlayButtonClick}>
           Play
         </button>
         <button className={styles.CreateRoomButton}>Create Custom Room</button>
