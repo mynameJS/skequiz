31c266fdc6eb15a010f03654113a30f88d715491
JA_X <envijs93@gmail.com>
2024-08-25 02:59:26 +0900

    feat: 실시간 통신을 위한 firebase RealTime Database 초기화

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, FirebaseApp } from 'firebase/app';
 import { getAnalytics, Analytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
@@ -10,6 +11,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID as string,
   appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID as string,
+  databaseURL: import.meta.env.VITE_FIREBASE_REALTIME_DATABASE_URL as string,
 };
 
 // Firebase 초기화
@@ -17,6 +19,9 @@ const app: FirebaseApp = initializeApp(firebaseConfig);
 const analytics: Analytics | undefined = getAnalytics(app);
 
 // FireStore DB 초기화
-const db = getFirestore(app);
+const fireStoreDB = getFirestore(app);
 
-export { app, analytics, db };
+// RealTime DB 초기화
+const realTimeDB = getDatabase(app);
+
+export { app, analytics, fireStoreDB, realTimeDB };
