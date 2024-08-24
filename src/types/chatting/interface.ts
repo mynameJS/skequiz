bcdbcefaf26051216a4ae7520da86e959e5a7565
JA_X <envijs93@gmail.com>
2024-08-25 02:59:56 +0900

    feat: 채팅 관련 type , interface 선언

---
+++ src/types/chatting/interface.ts
@@ -0,0 +1,5 @@
+export interface ChattingMessageData {
+  roomId?: string;
+  nickName: string;
+  message: string;
+}
---
+++ src/types/chatting/type.ts
@@ -0,0 +1,1 @@
+export type MessageListTuple = [string, { nickName: string; message: string }];
