7cba64292fe9e61747bf6e635337326bc9847292
JA_X <envijs93@gmail.com>
2024-08-29 14:39:18 +0900

    feat: 게임룸 입장 전 입장가능한 게임룸확인하는 페이지 구현

---
+++ src/pages/searchingRoom/searchingRoom.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { createChattingRoom, getExistingRoomData } from '../../services/sketchRoomService';
+import { userStore } from '../../store/userStore';
+import createShortUniqueId from '../../utils/createShortUniqueId';
+import useNavigateClick from '../../hooks/useNavigateClick';
+import styles from './searchingRoom.module.scss';
+
+const SearchingRoom = () => {
+  const [openRoomList, setOpenRoomList] = useState<string[] | string>([]);
+  console.log(openRoomList);
+  const navigateTo = useNavigateClick();
+
+  const updateCurrentRoomId = userStore(state => state.updateRoomId);
+
+  const updateOpenRoomList = (currentOpenRoomList: string[] | string) => {
+    setOpenRoomList(currentOpenRoomList);
+  };
+
+  useEffect(() => {
+    getExistingRoomData(updateOpenRoomList);
+  }, []);
+
+  useEffect(() => {
+    const selectingRoom = async () => {
+      if (openRoomList === 'create') {
+        const newRoomId = createShortUniqueId();
+        updateCurrentRoomId(newRoomId);
+        await createChattingRoom(newRoomId);
+        navigateTo('/sketchRoom');
+        return;
+      }
+
+      if (openRoomList.length) {
+        const targetRoomId = openRoomList[0];
+        updateCurrentRoomId(targetRoomId);
+        navigateTo('/sketchRoom');
+        return;
+      }
+    };
+    selectingRoom();
+  }, [openRoomList, navigateTo, updateCurrentRoomId]);
+  return <div className={styles.container}>플레이어 찾는중...</div>;
+};
+
+export default SearchingRoom;
