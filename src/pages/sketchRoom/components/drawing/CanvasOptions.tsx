ca99eac87098bf496fc04b3c22a6b74985035a67
JA_X <envijs93@gmail.com>
2024-08-31 15:54:10 +0900

    feat: 캔버스옵션 부분 컴포넌트화 및 스타일 작업

---
+++ src/pages/sketchRoom/components/drawing/CanvasOptions.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+import { Socket } from 'socket.io-client';
+import { COLOR_PRESET_TOP, COLOR_PRESET_BOTTOM, STROKE_SET } from '../../../../constant/canvasOptions';
+import { ContextOption } from '../../../../types/drawing/interface';
+import trashBin from '../../../../assets/trash_bin.svg';
+import styles from './CanvasOptions.module.scss';
+
+interface CanvasOptions {
+  contextOption: ContextOption;
+  socket: React.MutableRefObject<Socket>;
+  setContextOption: React.Dispatch<React.SetStateAction<ContextOption>>;
+  clearCanvas: () => void;
+}
+
+const CanvasOptions = ({ contextOption, socket, setContextOption, clearCanvas }: CanvasOptions) => {
+  const [isSelecting, setIsSelecting] = useState<boolean>(false);
+
+  const topColorArr = Object.values(COLOR_PRESET_TOP);
+  const bottomColorArr2 = Object.values(COLOR_PRESET_BOTTOM);
+
+  const selectContextOption = (optionName: string, selectedValue: string | number) => {
+    if (optionName === 'lineWidth') {
+      toggleIsSelecting();
+    }
+    setContextOption(prevContextOption => {
+      const newContextOption = {
+        ...prevContextOption,
+        [optionName]: selectedValue,
+      };
+      // 변경된 옵션을 소켓 서버에 전송
+      socket.current.emit('updateContextOption', newContextOption);
+      return newContextOption;
+    });
+  };
+
+  const toggleIsSelecting = () => {
+    setIsSelecting(prevValue => !prevValue);
+  };
+
+  return (
+    <div className={styles.options}>
+      <div className={styles.selectedColor} style={{ backgroundColor: contextOption.strokeStyle }}></div>
+      <div className={styles.selectColor}>
+        <div className={styles.colorList}>
+          {topColorArr.map(colorCode => (
+            <div
+              key={colorCode}
+              className={styles.topColorSwatch}
+              style={{ backgroundColor: colorCode }}
+              onClick={() => selectContextOption('strokeStyle', colorCode)}></div>
+          ))}
+        </div>
+        <div className={styles.colorList}>
+          {bottomColorArr2.map(colorCode => (
+            <div
+              key={colorCode}
+              className={styles.bottomColorSwatch}
+              style={{ backgroundColor: colorCode }}
+              onClick={() => selectContextOption('strokeStyle', colorCode)}></div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.selectLineWidth}>
+        <button onClick={toggleIsSelecting}>
+          <div
+            style={{
+              width: contextOption.lineWidth * 1,
+              height: contextOption.lineWidth * 1,
+            }}></div>
+        </button>
+        {isSelecting && (
+          <ul className={styles.lineWidthList}>
+            {STROKE_SET.map(width => (
+              <li key={width} className={styles.lineWidth} onClick={() => selectContextOption('lineWidth', width)}>
+                <div
+                  style={{
+                    width: width * 1,
+                    height: width * 1,
+                  }}></div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className={styles.deleteCanvas}>
+        <button onClick={clearCanvas}>
+          <img className={styles.trashBin} src={trashBin} alt="클리어" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CanvasOptions;
--- src/pages/sketchRoom/components/drawing/Drawing.tsx
+++ src/pages/sketchRoom/components/drawing/Drawing.tsx
@@ -1,9 +1,9 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import io, { Socket } from 'socket.io-client';
 import useSocketDrawing from '../../hook/useSocketDrawing';
 import useInitCanvas from '../../hook/useInitCanvas';
 import useResizeCanvas from '../../hook/useResizeCanvas';
-import { COLOR_PRESET_TOP, COLOR_PRESET_BOTTOM, STROKE_SET } from '../../../../constant/canvasOptions';
-import io from 'socket.io-client';
+import CanvasOptions from './canvasOptions';
 import { ContextOption } from '../../../../types/drawing/interface';
 import styles from './Drawing.module.scss';
 
@@ -16,45 +16,23 @@ const Drawing = ({ isMyTurn }: DrawingProps) => {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const [isDrawing, setIsDrawing] = useState(false);
   const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
-  const [isSelecting, setIsSelecting] = useState<boolean>(false);
+
   const [contextOption, setContextOption] = useState<ContextOption>({
     lineWidth: 2,
     strokeStyle: 'black',
   });
-
-  const toggleIsSelecting = () => {
-    setIsSelecting(prevValue => !prevValue);
-  };
-
-  const selectContextOption = (optionName: string, selectedValue: string | number) => {
-    if (optionName === 'lineWidth') {
-      toggleIsSelecting();
-    }
-
-    setContextOption(prevContextOption => {
-      const newContextOption = {
-        ...prevContextOption,
-        [optionName]: selectedValue,
-      };
-      // 변경된 옵션을 소켓 서버에 전송
-      socket.emit('updateContextOption', newContextOption);
-      return newContextOption;
-    });
-  };
-
-  // 이것도 나중에 useEffect로 빼기
-  const socket = io(import.meta.env.VITE_NODE_SOCKET_URL);
+  const socket = useRef<Socket>(io(import.meta.env.VITE_NODE_SOCKET_URL));
 
   useInitCanvas(canvasRef, setContext, contextOption);
   useResizeCanvas(canvasRef, containerRef);
-  useSocketDrawing({ socket, context, canvasRef, isMyTurn });
+  useSocketDrawing({ socket: socket.current, context, canvasRef, isMyTurn });
 
   const startDrawing = async (event: React.MouseEvent) => {
     if (context && isMyTurn) {
       context.beginPath();
       context.moveTo(event.nativeEvent.offsetX, event.nativeEvent.offsetY);
       setIsDrawing(true);
-      socket.emit('startDrawing'); // 선 시작 이벤트 전송
+      socket.current.emit('startDrawing'); // 선 시작 이벤트 전송
     }
   };
 
@@ -75,7 +53,7 @@ const Drawing = ({ isMyTurn }: DrawingProps) => {
         context.stroke();
 
         // 비율로 변환한 좌표 데이터를 서버에 전송
-        socket.emit('drawing', { x: relativeX, y: relativeY });
+        socket.current.emit('drawing', { x: relativeX, y: relativeY });
       }
     }
   };
@@ -84,7 +62,7 @@ const Drawing = ({ isMyTurn }: DrawingProps) => {
     if (isDrawing && context && isMyTurn) {
       context.closePath();
       setIsDrawing(false);
-      socket.emit('stopDrawing'); // 선 종료 이벤트 전송
+      socket.current.emit('stopDrawing'); // 선 종료 이벤트 전송
     }
   };
 
@@ -92,12 +70,17 @@ const Drawing = ({ isMyTurn }: DrawingProps) => {
     const canvas = canvasRef.current;
     if (canvas && context) {
       context.clearRect(0, 0, canvas.width, canvas.height); // 캔버스의 전체 영역 지우기
-      socket.emit('clearCanvas'); // 캔버스 초기화 이벤트 전송
+      socket.current.emit('clearCanvas'); // 캔버스 초기화 이벤트 전송
     }
   };
 
-  const colorArr = Object.values(COLOR_PRESET_TOP);
-  const colorArr2 = Object.values(COLOR_PRESET_BOTTOM);
+  useEffect(() => {
+    const socketInstance = socket.current;
+
+    return () => {
+      socketInstance.disconnect();
+    };
+  }, []);
 
   return (
     <div className={styles.container} ref={containerRef}>
@@ -109,48 +92,12 @@ const Drawing = ({ isMyTurn }: DrawingProps) => {
         onMouseUp={stopDrawing}
         onMouseLeave={stopDrawing}
       />
-      <div className={styles.options}>
-        <div className={styles.selectedColor} style={{ backgroundColor: contextOption.strokeStyle }}></div>
-        <div className={styles.selectColor}>
-          <div className={styles.colorList}>
-            {colorArr.map(colorCode => (
-              <div
-                key={colorCode}
-                className={styles.colorSwatch}
-                style={{ backgroundColor: colorCode }}
-                onClick={() => selectContextOption('strokeStyle', colorCode)}></div>
-            ))}
-          </div>
-          <div className={styles.colorList}>
-            {colorArr2.map(colorCode => (
-              <div
-                key={colorCode}
-                className={styles.colorSwatch}
-                style={{ backgroundColor: colorCode }}
-                onClick={() => selectContextOption('strokeStyle', colorCode)}></div>
-            ))}
-          </div>
-        </div>
-        <div className={styles.selectLineWidth}>
-          <button onClick={toggleIsSelecting}>선굵기</button>
-          {isSelecting && (
-            <ul className={styles.lineWidthList}>
-              {STROKE_SET.map(width => (
-                <li key={width} className={styles.lineWidth} onClick={() => selectContextOption('lineWidth', width)}>
-                  <div
-                    style={{
-                      width: width * 2,
-                      height: width * 2,
-                    }}></div>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div className={styles.deleteCanvas}>
-          <button onClick={clearCanvas}>캔버스 롤백</button>
-        </div>
-      </div>
+      <CanvasOptions
+        contextOption={contextOption}
+        socket={socket}
+        setContextOption={setContextOption}
+        clearCanvas={clearCanvas}
+      />
     </div>
   );
 };
