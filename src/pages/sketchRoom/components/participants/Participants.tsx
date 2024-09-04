1a49aca906b093f596423834451abcfa2b2e7f69
JA_X <envijs93@gmail.com>
2024-09-05 05:19:56 +0900

    feat: 참여자 리스트 출제자, 정답자 구분 스타일링 로직 적용

--- src/pages/sketchRoom/components/participants/Participants.tsx
+++ src/pages/sketchRoom/components/participants/Participants.tsx
@@ -6,15 +6,39 @@ interface ParticipantsProps {
   userId: string;
   currentUserId: string;
   roomOwnerId: string;
+  currentDrawerId: string;
+  isPlaying: boolean;
+  isAnswer: boolean;
+  totalScore: number;
 }
 
-const Participants = ({ nickName, userId, currentUserId, roomOwnerId }: ParticipantsProps) => {
+const Participants = ({
+  nickName,
+  userId,
+  currentUserId,
+  roomOwnerId,
+  currentDrawerId,
+  isPlaying,
+  isAnswer,
+  totalScore,
+}: ParticipantsProps) => {
   const isYou = userId === currentUserId;
   const checkOwner = roomOwnerId === userId;
+  const isCurrentDrawer = currentDrawerId === userId;
+
+  const containerClassName = `${styles.container} ${
+    isPlaying ? (isCurrentDrawer ? styles.drawerNickName : isAnswer ? styles.answerNickName : '') : ''
+  }`;
+
   return (
-    <div className={styles.nickName}>
-      {checkOwner && <img className={styles.owner} src={crown} alt="방장" />}
-      {nickName} {isYou && <span>(You)</span>}
+    <div className={containerClassName}>
+      <div className={styles.nickNameBox}>
+        {checkOwner && <img className={styles.owner} src={crown} alt="방장" />}
+        <p>
+          {nickName} {isYou && <span>(You)</span>}
+        </p>
+      </div>
+      <p>{totalScore} point .</p>
     </div>
   );
 };
