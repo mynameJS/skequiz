09ab4c36093e32eba3e3316165193a5de8e69e10
JA_X <envijs93@gmail.com>
2024-09-05 18:29:07 +0900

    fix: 빌드 과정에서 에러 처리를 위한 컴포넌트 import 부분 .tsx 확장자 붙여주기

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 import Root from './Root';
-import Landing from './landing/Landing';
-import SketchRoom from './sketchRoom/SketchRoom';
+import Landing from './landing/Landing.tsx';
+import SketchRoom from './sketchRoom/SketchRoom.tsx';
 
 export { Root, Landing, SketchRoom };
