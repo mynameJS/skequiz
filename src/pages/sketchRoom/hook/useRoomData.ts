4821591b4888559c4a98132beb05f9f61c801588
JA_X <envijs93@gmail.com>
2024-09-10 20:13:29 +0900

    feat: 방 삭제 로직 임시 삭제

--- src/pages/sketchRoom/hook/useRoomData.ts
+++ src/pages/sketchRoom/hook/useRoomData.ts
@@ -4,7 +4,6 @@ import {
   getParticipantsData,
   joinParticipant,
   leaveParticipant,
-  deletePlayRoom,
 } from '../../../services/sketchRoomService';
 import { UserDataType } from '../../../types/user/interface';
 import { playSound } from '../../../utils/playSound';
@@ -13,8 +12,7 @@ const useRoomData = (
   currentRoomId: string,
   currentUserNickName: string,
   currentUserId: string,
-  updateParticipantList: (participants: UserDataType[]) => void,
-  currentParticipantListLength: number
+  updateParticipantList: (participants: UserDataType[]) => void
 ) => {
   useEffect(() => {
     const fetchRoomData = async () => {
@@ -42,8 +40,7 @@ const useRoomData = (
     // 먼저 실행된 sendChattingMessage 함수만 실행되는 이슈 발생
     // 검색해보니 beforeunload 이벤트에 비동기처리 시 모든 비동기처리가 실행된다는 보장이 없다고함 (다 실행되기 전에 페이지 닫히면)
     // 첫번쨰 비동기처리는 실행되는 걸 단서삼아 Promise.all 로 하나의 await 키워드 안에서 모두 실행시켜서 일단은 정상작동되게 만듦
-    const handleUnload = async (currentParticipantListLength: number) => {
-      console.log('남은인원', currentParticipantListLength);
+    const handleUnload = async () => {
       try {
         await Promise.all([
           sendChattingMessage(currentRoomId, {
@@ -57,7 +54,7 @@ const useRoomData = (
       }
     };
     // 브라우저 종료 또는 새로고침 감지
-    window.addEventListener('beforeunload', () => handleUnload(currentParticipantListLength));
+    window.addEventListener('beforeunload', handleUnload);
 
     return () => {
       const sendLeaveMessage = async () => {
@@ -70,7 +67,7 @@ const useRoomData = (
       sendLeaveMessage();
       playSound('leaveRoom');
       // 이벤트 핸들러 구독 취소
-      window.removeEventListener('beforeunload', () => handleUnload(currentParticipantListLength));
+      window.removeEventListener('beforeunload', handleUnload);
     };
 
     // 의존성 일단 보류
