ef8bbd4d3b17b91b34686abfe89a31986bfdc99e
JA_X <envijs93@gmail.com>
2024-09-12 21:55:59 +0900

    feat: 사설방 입장 로직 구현

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,14 +1,21 @@
 import { useState } from 'react';
-import { createRandomNickName } from '../../utils/createRandomNickName';
 import { userStore } from '../../store/userStore';
+import createShortUniqueId from '../../utils/createShortUniqueId';
 import SearchingRoom from './components/searchingRoom/SearchingRoom';
+import useNavigateClick from '../../hooks/useNavigateClick';
+import { createRandomNickName } from '../../utils/createRandomNickName';
+import { createChattingRoom } from '../../services/sketchRoomService';
 import styles from './Landing.module.scss';
 
 const Landing = () => {
   const [userNickName, setUserNickName] = useState<string>('');
   const [isSearching, setIsSearching] = useState<boolean>(false);
 
+  const navigateTo = useNavigateClick();
+
   const updateCurrentUserNickName = userStore(state => state.updateUserNickName);
+
+  const updateCurrentRoomId = userStore(state => state.updateRoomId);
 
   const handleUserNickNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserNickName(e.target.value);
@@ -30,6 +37,25 @@ const Landing = () => {
     setIsSearching(prevValue => !prevValue);
   };
 
+  // 사설방 개설 시 isPublic false 로 변환 후 바로 입장
+  const handleCreatePrivateRoomButtonClick = async () => {
+    // 닉네임을 입력하지 않고 플레이버튼 클릭 시 랜덤닉네임 부여
+    if (userNickName.trim() === '') {
+      const randomNickName = createRandomNickName();
+      updateCurrentUserNickName(randomNickName);
+    } else {
+      updateCurrentUserNickName(userNickName);
+    }
+    const newRoomId = createShortUniqueId();
+    updateCurrentRoomId(newRoomId);
+    // 사설방 개설
+    await createChattingRoom(newRoomId, false);
+    // 사설방 입장
+    setTimeout(() => {
+      navigateTo('/sketchRoom');
+    }, 1000);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.registerBox}>
@@ -43,7 +69,9 @@ const Landing = () => {
             <button className={styles.playButton} onClick={handlePlayButtonClick}>
               Play
             </button>
-            <button className={styles.createRoomButton}>Create Private Room</button>
+            <button className={styles.createRoomButton} onClick={handleCreatePrivateRoomButtonClick}>
+              Create Private Room
+            </button>
           </div>
         </div>
         {isSearching && <SearchingRoom />}
