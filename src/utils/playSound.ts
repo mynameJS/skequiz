621262bc3ee196729a61cc0f6f712889829f34d6
JA_X <envijs93@gmail.com>
2024-09-27 20:11:00 +0900

    feat: 커스텀 룸 옵션 선택 시 사운드 추가

--- src/utils/playSound.ts
+++ src/utils/playSound.ts
@@ -1,11 +1,13 @@
 import EnterRoom from '../assets/sound/enter_room.mp3';
 import LeaveRoom from '../assets/sound/leave_room.mp3';
 import CorrectAnswer from '../assets/sound/correctAnswer.mp3';
+import SelectOption from '../assets/sound/pop.mp3';
 
 const sounds = {
   enterRoom: { fileName: EnterRoom, volume: 1 },
   leaveRoom: { fileName: LeaveRoom, volume: 1 },
   correctAnswer: { fileName: CorrectAnswer, volume: 1 },
+  selectOption: { fileName: SelectOption, volume: 1 },
 };
 
 export function playSound(soundName: keyof typeof sounds) {
